c653c37787a8c2f537016b1deb117923cd9d7af7
DhvaniCA <Info@edquezt.co.in>
2026-05-11 13:35:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     AWS_ACCESS_KEY_ID     = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     AWS_REGION            = os.getenv("AWS_REGION", "us-east-1")
-    S3_BUCKET_NAME        = os.getenv("S3_BUCKET_NAME")
+    AWS_S3_BUCKET        = os.getenv("S3_BUCKET_NAME")
 
     # MongoDB
     MONGODB_URI     = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
